4ac0537eef96fa69a76f7adc8bbc13f6f25bbf27
Rawnak Sarker <eltonrawn@gmail.com>
2020-08-17 23:37:08 +0600

    Initialize with spring

---
+++ src/main/java/com/onizuka/cfguide/CfguideApplication.java
@@ -0,0 +1,13 @@
+package com.onizuka.cfguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfguideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfguideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onizuka/cfguide/CfguideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onizuka.cfguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfguideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
